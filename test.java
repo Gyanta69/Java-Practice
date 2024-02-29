8d2ca3ce447f06d79d89f90a53e36ce9551b05c9
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-02-29 07:19:58 +0545

    Java Practice

---
+++ test.java
@@ -0,0 +1,9 @@
+// Here public is an access modifier 
+// test is the class name
+public class test {
+    public static void main(String[]args) {
+
+        System.out.println("Hello World!\n");// \n to break a line and print output
+        System.out.println("Hello World!");
+    }
+}
---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    String A = "Pragyan";
+    int  a = 10;
+    int b = 20;
+    int sum = a+b;
+    double price = 22.2 ;
+    public static void main(String[]args){
+        System.out.println("The sum is:", + sum);
+
+    }
+}

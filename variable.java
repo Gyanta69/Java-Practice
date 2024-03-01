3bd74f7efbae215e60e598032d7b510c6f4d5d2b
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-03-01 11:09:06 +0545

    Some more Variables

--- variable.java
+++ variable.java
@@ -1,11 +1,19 @@
 public class variable {
-    String A = "Pragyan";
-    int  a = 10;
-    int b = 20;
-    int sum = a+b;
-    double price = 22.2 ;
-    public static void main(String[]args){
-        System.out.println("The sum is:", + sum);
+  public static void main(String[]args){
+        String c = "Pragyan";
+        int  a = 10;
+        int b = 20;
+        int sum = a+b;
+        double price = 22.2 ;
+        boolean s = true;
+        char pragyan = 'C' ;
+        System.out.println("The sum is:"+sum);
+        System.out.println(c);
+        int multiply = a*b;
+        System.out.println("The multiplication is :"+multiply);
+        System.out.println(price);
+        System.out.println(s);
+        System.out.println(pragyan);
 
     }
 }

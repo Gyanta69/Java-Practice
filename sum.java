9cc2e1716f46a32ef44692377aa6f8581e864a73
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-03-01 11:32:23 +0545

    Sum of two values

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int firstValue = sc.nextInt();
+        int secondValue = sc.nextInt();
+        int sum = firstValue + secondValue ;
+        System.out.println("The addition of two values is :" +sum);
+        sc.close();
+
+    }
+}

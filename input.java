57091ec6ea7ad1ac289f55d335891bd1fa8e273e
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-03-01 11:33:24 +0545

    Learning to input values in Java

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();//nextLine used to take a whole line as an input
+        //nextInt() for integer
+        //nextfloat() for decimal values
+        System.out.println(name);
+        sc.close();
+    }
+}
+
+  
+

434b6fcd4957e2d68a01c8d5653c1c3275767f11
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-03-01 11:34:42 +0545

    To input 2 values

---
+++ input2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class input2 {
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        String address = sc.next();
+        System.out.println("Input Your name:"+ name  + "Input Your address:" + address);
+        sc.close();
+    }
+    
+}

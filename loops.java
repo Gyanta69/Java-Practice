47eb0d0365bf1ba0a106b598a01ea780e799d9d5
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-03-03 10:04:38 +0545

    Intro to Loops

---
+++ loops.java
@@ -0,0 +1,15 @@
+// public class loops {
+//     public static void main(String[] args) {
+//         for(int counter = 0 ; counter < 11 ; counter++){
+//             System.out.println("Hello world");
+//         }
+//     }
+// }
+public class loops {
+    public static void main(String[] args) {
+        for(int i = 0 ; i < 11 ; i++){
+            System.out.println(i);
+        }
+    }
+}
+
